e5adc31e37c63bf99e32595dc54ad8237a19d072
johnedstone <johnedstone@gmail.com>
2017-01-10 19:35:45 +0000

    switched to django 1.9.12 and put defaults in for os.environ.get

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -8,9 +8,9 @@ SECRET_KEY = os.environ.get(
      hashlib.sha1(os.urandom(128)).hexdigest(), 
 )
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
 ADMIN_URL = os.environ.get('ADMIN_URL', 'admin')
-INTERNAL_IPS = os.environ.get('INTERNAL_IPS', 'localhost,127.0.0.1').split(',') 
+INTERNAL_IPS = os.environ.get('INTERNAL_IPS', 'localhost 127.0.0.1').split() 
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -84,13 +84,8 @@ SITE_ID = 1
 # 30 min
 SESSION_COOKIE_AGE = 60 * 30
 
-# Start: Used to go directly to the OP 
-# LOGIN_URL='myproject_login'
-OP_NAME = os.environ.get('OP_NAME')
-# Some day figure out how to do this with URL Namespace
+OP_NAME = os.environ.get('OP_NAME', 'unknown_op_name')
 LOGIN_URL = '/openid/openid/{}'.format(OP_NAME)
-# LOGIN_REDIRECT_URL = 'myapp:private'
-# End: Used to go directly to the OP 
 
 # djangooidc
 AUTHENTICATION_BACKENDS = [
@@ -98,30 +93,28 @@ AUTHENTICATION_BACKENDS = [
 'djangooidc.backends.OpenIdConnectBackend',
 ]
 
-OP_NAME = os.environ.get('OP_NAME')
 
 BEHAVIOR = {
     "response_type": "code",
-    # "scope": os.environ.get('SCOPE').split(',')
-    "scope": os.environ.get('SCOPE').split() # origin/openshift can't handle comma
+    "scope": os.environ.get('SCOPE', 'unknown scope').split() # origin/openshift can't handle comma
 }
 
 PROVIDER_INFO = {
-    'authorization_endpoint': os.environ.get('AUTHORIZATION_ENDPOINT'),
-    'token_endpoint': os.environ.get('TOKEN_ENDPOINT'), 
-    'userinfo_endpoint': os.environ.get('USERINFO_ENDPOINT'), 
-    'issuer': os.environ.get('ISSUER'), 
-    'jwks_uri': os.environ.get('JWKS_URI'), 
+    'authorization_endpoint': os.environ.get('AUTHORIZATION_ENDPOINT', 'unknown_auth_endpoint'),
+    'token_endpoint': os.environ.get('TOKEN_ENDPOINT', 'unknown_token_endpoint'), 
+    'userinfo_endpoint': os.environ.get('USERINFO_ENDPOINT', 'unknown_userinfo_endpoint'), 
+    'issuer': os.environ.get('ISSUER', 'unknown_issuer'), 
+    'jwks_uri': os.environ.get('JWKS_URI', 'unknown_jwks_uri'), 
 }
 
 OIDC_PROVIDERS = {
     OP_NAME: {
         'behaviour': BEHAVIOR,
         'client_registration': {
-            'client_id': os.environ.get('CLIENT_ID'),
-            'client_secret': os.environ.get('CLIENT_SECRET'),
-            'redirect_uris': os.environ.get('REDIRECT_URIS').split(','),
-            'post_logout_redirect_uris': os.environ.get('POST_LOGOUT_REDIRECT_URIS').split(','),
+            'client_id': os.environ.get('CLIENT_ID', 'unknown_id'),
+            'client_secret': os.environ.get('CLIENT_SECRET', 'unknown_secret'),
+            'redirect_uris': os.environ.get('REDIRECT_URIS', 'unknown_uris').split(','),
+            'post_logout_redirect_uris': os.environ.get('POST_LOGOUT_REDIRECT_URIS', 'unknown_post_uris').split(','),
         },
         'provider_info': PROVIDER_INFO,
     },
